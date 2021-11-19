50855e0a5030d9d585e1becf3e3e7620cbef295a
ML-Design-web <93223257+ML-Design-web@users.noreply.github.com>
2021-11-19 17:06:52 +0100

    lots of updates

---
+++ tests_locals/test_flex_homepage.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <title>Accueil | La glisse</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="#" />
+    <meta name="robots" content="index, follow" />
+    <meta property="theme-color" content="#B0FF00" />
+    <meta property="og:title" content="Accueil | La glisse" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="#" />
+    <meta property="og:url" content="#" />
+    <meta property="og:description" content="#" />
+    <meta property="og:locale" content="fr_FR" />
+    <meta property="keywords" content="glisse, skateboard, rollers, neige, sport" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Accueil | La glisse" />
+    <meta name="twitter:description" content="#" />
+    <meta name="twitter:image" content="#" />
+
+
+    <!-- Links -->
+    <link rel="stylesheet" href="style.css" />
+    <!--<link href="assets/img/favicon.png" rel="icon">-->
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="main_img">
+        <div class="img1">
+            <img src="img/img-surf.webp" alt="surf" id="surf">
+        </div>
+
+        <div class="img2">
+            <img src="img/img-ski.webp" alt="ski" id="ski">
+        </div>
+    </div>
+
+    <div class="container">
+
+        <div class="flex_homepage">
+            <span class="left" id="winter">SPORT D'ÉTÉ</span>
+            <img src="img/chevron-compact-down.svg" alt="flèche" class="left">
+        </div>
+
+        <div class="flex_homepage2">
+            <span class="right" id="summer">SPORT D'HIVER</span>
+            <img src="img/chevron-compact-down.svg" alt="flèche" class="right">
+        </div>
+    </div>
+
+
+</body>
+
+</html>
