8ea8e54f3d818a83b01b55483644c25471e0dbb5
Cresus83 <alan.thob@hotmail.fr>
2021-11-26 17:28:10 +0100

    Ajout des 2 versions du site

---
+++ ancienne-version/tests_locals/test_flex_homepage.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <title>Accueil | La glisse</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="#" />
+    <meta name="robots" content="index, follow" />
+    <meta property="theme-color" content="#B0FF00" />
+    <meta property="og:title" content="Accueil | La glisse" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="#" />
+    <meta property="og:url" content="#" />
+    <meta property="og:description" content="#" />
+    <meta property="og:locale" content="fr_FR" />
+    <meta property="keywords" content="glisse, skateboard, rollers, neige, sport" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Accueil | La glisse" />
+    <meta name="twitter:description" content="#" />
+    <meta name="twitter:image" content="#" />
+
+
+    <!-- Links -->
+    <!--<link href="assets/img/favicon.png" rel="icon">-->
+    <link href="../assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="../assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../assets/css/style.css" />
+    <link rel="stylesheet" href="navbar_style.css" />
+
+</head>
+
+<body>
+
+    <?php
+    include_once('test_navbar.php');
+    ?>
+    <!-- <section>
+
+        <div class="main_img">
+            <div class="img1">
+                <img src="img/img-surf.webp" alt="surf" id="surf">
+            </div>
+
+            <div class="img2">
+                <img src="img/img-ski.webp" alt="ski" id="ski">
+            </div>
+        </div>
+
+        <div class="container_season">
+
+            <div class="flex_homepage">
+                <span class="left" id="winter">SPORT D'ÉTÉ</span>
+                <img src="img/chevron_summer.svg" alt="flèche" class="left">
+            </div>
+
+            <div class="flex_homepage2">
+                <span class="right" id="summer">SPORT D'HIVER</span>
+                <img src="img/chevron_winter.svg" alt="flèche" class="right">
+            </div>
+        </div>
+    </section> -->
+
+
+    <section id="hero">
+        <div class="hero-container">
+            <div class="container mt-5  ">
+                <div class="row wd-100 mt-5">
+                    <div class="col-md-6 text-center mx-auto pr-25 mt-5">
+                        <a href="#about" class="btn-get-started">sport d'été'</a>
+                    </div>
+                    <div class="col-md-6 text-center mx-auto mt-5">
+                        <a href="#about" class="btn-get-started">sport d'hiver'</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="container">
+            <div class="row about-container">
+                <div class="col-sm-6 img_content">
+                    <div class="img1">
+                        <img src="img/img-surf.webp" alt="surf" id="surf">
+                    </div>
+                    <div class="flex_homepage">
+                        <span class="left" id="winter">SPORT D'ÉTÉ</span>
+                        <img src="img/chevron_summer.svg" alt="flèche" class="left">
+                    </div>
+                </div>
+                <div class="col-sm-6 img_content">
+                    <div class="img2">
+                        <img src="img/img-ski.webp" alt="ski" id="ski">
+                    </div>
+                    <div class="flex_homepage2">
+                        <span class="right" id="summer">SPORT D'HIVER</span>
+                        <img src="img/chevron_winter.svg" alt="flèche" class="right">
+                    </div>
+                </div>
+            </div>
+        </div> -->
+    </section>
+
+    <main>
+        <section id="été">
+
+        </section>
+
+
+
+        <section id="hiver">
+
+        </section>
+
+
+
+        <section id="contact">
+
+        </section>
+    </main>
+
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="copyright">
+                &lt;/&gt; avec ❤ par <strong>Alan THOB</strong> & <strong>Mathis Lambert</strong><br>
+                <strong><a href="index.php">Le Monde De La Glisse</a> 2021 - 2022.</strong><br>
+            </div>
+        </div>
+    </footer><!-- End Footer -->
+
+
+</body>
+
+</html>
---
+++ nouvelle-version/_footer/_footer.php
@@ -0,0 +1,197 @@
+<!-- ======= Footer ======= -->
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title></title>
+    <style>
+        #footer {
+            background: black;
+            padding: 0 0 15px 0;
+            color: #fff;
+            font-size: 14px;
+        }
+
+        #footer .footer-top {
+            background: #151515;
+            border-bottom: 1px solid #222222;
+            padding: 60px 0 30px 0;
+        }
+
+        #footer .footer-top .footer-info {
+            margin-top: 50px;
+        }
+
+        #footer .footer-top .social-links a {
+            font-size: 18px;
+            display: inline-block;
+            background: #292929;
+            color: #fff;
+            line-height: 1;
+            padding: 8px 0;
+            margin-right: 4px;
+            border-radius: 4px;
+            text-align: center;
+            width: 36px;
+            height: 36px;
+            transition: 0.3s;
+        }
+
+        #footer .footer-top .social-links a:hover {
+            background-color: #fff;
+            color: #151515;
+            text-decoration: none;
+        }
+
+        #footer .footer-top h4 {
+            font-size: 16px;
+            font-weight: 600;
+            color: #fff;
+            position: relative;
+            padding-bottom: 12px;
+        }
+
+        #footer .footer-top .footer-links {
+            margin-bottom: 30px;
+        }
+
+        #footer .footer-top .footer-links ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        #footer .footer-top .footer-links ul i {
+            padding-right: 2px;
+            color: #000000;
+            font-size: 18px;
+            line-height: 1;
+        }
+
+        #footer .footer-top .footer-links ul li {
+            padding: 10px 0;
+            display: flex;
+            align-items: center;
+        }
+
+        #footer .footer-top .footer-links ul li:first-child {
+            padding-top: 0;
+        }
+
+        #footer .footer-top .footer-links ul a {
+            color: #fff;
+            transition: 0.3s;
+            display: inline-block;
+            line-height: 1;
+            cursor: pointer;
+        }
+
+        /* Copyright */
+
+        #footer .footer-top .footer-linksul a:hover {
+            color: #3fa9f5;
+        }
+
+        #footer .copyright {
+            text-align: center;
+            padding-top: 15px;
+        }
+
+        #footer .copyright a{
+            color: #fff;
+        }
+        #footer .copyright a:hover{
+            color: #3fa9f5;
+        }
+
+
+        @media only screen and (max-width: 991px) {
+            #footer .social-links {
+                margin-bottom: 30px;
+            }
+        }
+
+        @media only screen and (max-width: 576px) {
+            #footer .header-theme {
+                transform: translatex(0px);
+            }
+        }
+    </style>
+</head>
+<body>
+<footer id="footer">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer-info">
+                        <a href="#"><img src="https://www.sfait.net/images/la-glisse/logo.png" alt="logo-le-monde-de-la-glisse" class="img-fluid" width="210"></a>
+                        <div class="social-links mt-3">
+                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="#" class="youtube"><i class="bx bxl-youtube"></i></a>
+                            <a href="#" class="discord"><i class="bx bxl-discord"></i></a>
+                            <a href="#" class="mail"><i class='bx bx-mail-send'></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Vêtements par catégorie</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Homme</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Femme</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Enfants</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Bons plans</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Accessoires par catégorie</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Assistance</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Nos services</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Nos produits</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Suivi de commande</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Centre de téléchargements</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Juridique</h4>
+                    <ul>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">CGU</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">CGV</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Cookies</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div id="copyright" class="copyright">
+            &lt;/&gt; avec ❤ par <strong><span><a href="https://github.com/Cresus83" target="_blank">THOB Alan</a></strong> & <strong><a href="https://github.com/ML-Design-web/" target="_blank">LAMBERT Mathis</a></span>.<br>
+                &copy; Copyright<a href="#"> Le monde de la glisse</a> #DATE#.
+        </div>
+    </div>
+</footer><!-- End Footer -->
+
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+    <i class="bi bi-arrow-up-short"></i>
+</a>
+</body>
+
+<!-- Vendor JS Files -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://www.sfait.net/assets/vendor/aos/aos.js"></script>
+<script src="https://www.sfait.net/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://www.sfait.net/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="https://www.sfait.net/assets/vendor/php-mail-form/validate-mail.js"></script>
+<script src="https://www.sfait.net/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="https://www.sfait.net/assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+<!-- Template Main JS File -->
+<script src="https://www.sfait.net/assets/js/main.js"></script>
+<script>document.getElementById(`copyright`).innerHTML = document.getElementById(`copyright`).innerHTML.replace(`#DATE#`, new Date().getFullYear());</script>
+</html>
