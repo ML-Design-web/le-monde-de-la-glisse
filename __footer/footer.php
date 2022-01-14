84e4049088a320d24774b3786de1ca8c3ce27ca2
Cresus83 <alan.thob@hotmail.fr>
2022-01-14 19:03:47 +0100

    add conditions on footer

--- __footer/footer.php
+++ __footer/footer.php
@@ -20,22 +20,22 @@
                 </div>
 
                 <div class="col-lg-2 col-md-6 footer-links">
-                    <h4>Useful Links</h4>
+                    <h4>Liens annexes</h4>
                     <ul>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Accueil</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Tutoriel</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Météo</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Nous contacter</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Mon espace client</a></li>
                     </ul>
                 </div>
 
                 <div class="col-lg-3 col-md-6 footer-links">
-                    <h4>Our Services</h4>
+                    <h4>La boutique</h4>
                     <ul>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                        <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Vêtements par catégorie</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Accessoires par catégorie</a></li>
+                        <li><i class="bx bx-chevron-right"></i> <a href="#">Nos bons plans</a></li>
                         <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
                         <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
                     </ul>
@@ -59,15 +59,20 @@
         <div class="copyright-wrap d-md-flex py-4">
             <div class="me-md-auto text-center text-md-start">
                 <div class="copyright">
-                    &copy; Copyright <strong><span>LA GLISSE</span></strong>. Tous droits réservés
+                    &copy; Copyright <strong><span>LA GLISSE</span></strong>. Tous droits réservés.
                 </div>
             </div>
+
+            <div class="me-md-auto text-center text-md-middle">
+                <a class="footer-condition" href="#null">Conditions Générales de Vente</a> | <a class="footer-condition" href="#null">Conditions Générales d'Utilisation</a>
+            </div>
+
             <div class="social-links text-center text-md-right pt-3 pt-md-0">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                <a href="#null" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#null" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#null" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#null" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#null" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
         </div>
 
