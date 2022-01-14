3b47f7f59806260ad07dea1f7902da66d1a5c455
Cresus83 <alan.thob@hotmail.fr>
2022-01-14 11:39:34 +0100

    removed  useless js in some pages

--- shop/women-clothes.php
+++ shop/women-clothes.php
@@ -376,7 +376,6 @@ include_once('../__footer/footer.php');
 <div id="preloader"></div>
 
 <!-- Vendor JS Files -->
-<script src="../assets/vendor/purecounter/purecounter.js"></script>
 <script src="../assets/vendor/aos/aos.js"></script>
 <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="../assets/vendor/glightbox/js/glightbox.min.js"></script>
